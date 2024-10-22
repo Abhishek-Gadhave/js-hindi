0c53f29eba43815e2f8cfa1adfb3c4067b2273df
Abhishek-Gadhave <84014345+Abhishek-Gadhave@users.noreply.github.com>
2024-10-22 10:12:15 +0000

    Datatypes

---
+++ 01_Basics/02_dataType.js
@@ -0,0 +1,20 @@
+"use strict"  //it will treat all version of code is newer version
+
+//alert(3+3); //it will not work as we are using node version not browser.
+
+console.log(3+3);
+
+//primitive data types
+/*
+Number,bigInit,string,
+boolean,null,undefined,symbol */
+
+
+//Non-Primitive Data type
+console.log(typeof "hitrsh")
+
+const age = 43;
+console.log(typeof age)
+
+console.log(typeof null);
+//data type of null is object
