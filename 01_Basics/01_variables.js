53d29f0baf4528a30b07571be3742ab5163aa771
Abhishek-Gadhave <84014345+Abhishek-Gadhave@users.noreply.github.com>
2024-10-22 09:46:45 +0000

    How let const and var works behind the scense

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 143523;
+let accountName = "Abhishek Gadhave";
+var accountPassword = "Abhu123*#";
+accountCity = "Pune"
+let accountState;
+
+//accountId = 2;
+
+/*PREFER NOT TO USE 
+VAR Beacause of block scope and functional scope*/
+accountName = "Akshay bhoir";
+accountPassword = "Akshu123#*";
+accountCity="Wai";
+console.table([accountId,accountName,accountPassword,accountCity,accountState])
